ff6be7d5d259edd21929b95c216d66a64a5452ab
nasrlol <nsrddyn@gmail.com>
2024-09-04 01:50:48 +0200

    complete redisgin of the page trying to add device detection failing completely at the moment resorted the pages into a new folder and more

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
-function getscreenres(screenres) {
+function getDeviceType() {
+    const USER_AGENT = navigator.userAgent.toLowerCase();
 
-    var screenres = screen.width + "x" + screen.height
-    
+    let DEVICE_TYPE = '';
+
+    if (USER_AGENT.indexOf('mobile') !== -1) {
+        DEVICE_TYPE = 'Mobile';
+    } else if (USER_AGENT.indexOf('tablet') !== -1) {
+        DEVICE_TYPE = 'Tablet';
+    } else {
+        DEVICE_TYPE = 'Desktop';
+    }
+
+    const currentURL = window.location.href;
+
+    if (DEVICE_TYPE === 'Mobile' && !currentURL.includes("mobileindex.html")) {
+        window.location.href = "mobileindex.html?redirected=true";
+    } else if (DEVICE_TYPE !== 'Mobile' && !currentURL.includes("index.html")) {
+        window.location.href = "index.html?redirected=true";
+    }
 }
-getscreenres()
 
+getDeviceType();
