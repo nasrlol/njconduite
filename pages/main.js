de23e276cae2a45eb8691a319cbde44cecdacb6a
Abdellah El Morabit <76111727+nasrlol@users.noreply.github.com>
2024-09-14 22:15:10 +0200

    Refactor header and navigation links for consistency and improved styling

---
+++ pages/main.js
@@ -0,0 +1,7 @@
+// Dropdown toggle functionality
+document.querySelector('.dropdown').addEventListener('click', function () {
+    const dropdownMenu = document.querySelector('.dropdown-menu');
+    dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
+
+});
+
