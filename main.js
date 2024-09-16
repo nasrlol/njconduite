194ac562bfb2bdda4b1f3ef693aa53bde7acead3
Abdellah El Morabit <76111727+nasrlol@users.noreply.github.com>
2024-09-16 23:20:24 +0200

    Refactor banner button click event handling for improved functionality

--- main.js
+++ main.js
@@ -7,8 +7,23 @@ document.querySelector('.dropdown').addEventListener('click', function () {
 });
 
 document.querySelector('.banner-button-call').addEventListener('click', function () {
-
     const buttonSlide = document.querySelector('.banner-button-call-slide');
-    buttonSlide.style.display = buttonSlide.style.opacity === 1 ? 0 : 1; 
+    
+    // Check if the element is visible (opacity is 1)
+    if (buttonSlide.style.opacity === '1') {
+        buttonSlide.style.opacity = '0';  // Hide the button by setting opacity to 0
+        buttonSlide.style.display = 'none';  // Optionally hide the element using display property
+    } else {
+        buttonSlide.style.opacity = '1';  // Show the button by setting opacity to 1
+        buttonSlide.style.display = 'block';  // Optionally display the element using display property
+    }
+
+    if (buttonSlide.style.width === '0') {
+        buttonSlide.style.width = '150'
+    } else {
+        
+        buttonSlide.style.width = '0'
+    }
+
 
 });
