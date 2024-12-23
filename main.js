c03a4d55b17c375aa7a952dd52ae36ae1f59dbd1
Abdellah El Morabit <nsrddyn@gmail.com>
2024-12-23 20:02:29 +0100

    the basis for the first page is finished

--- main.js
+++ main.js
@@ -1,33 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-	const callButtonSlide = document.getElementById("call-slide");
-	const callButtonContainer = document.getElementById("call-container");
-	const callButton = document.getElementById("call-button");
-	if (callButton) {
-		callButton.addEventListener("click", function (event) {
-			event.stopPropagation(); // Prevent event from bubbling up
-			callButtonSlide.classList.toggle("expanded");
-		});
-	}
-
-	document.addEventListener("click", function (event) {
-		if (
-			!callButtonContainer.contains(event.target) &&
-			!callButtonSlide.contains(event.target) &&
-			callButtonSlide.classList.contains("expanded")
-		) {
-			callButtonSlide.classList.remove("expanded");
-		}
-	});
-
-	const menuButton = document.querySelector(".menu-button");
-	const sideNav = document.getElementById("dropdownMenu");
-
-	if (menuButton && sideNav) {
-		menuButton.addEventListener("click", function () {
-			sideNav.classList.toggle("open");
-		});
-	}
-
 	const observer = new IntersectionObserver((entries, observer) => {
 		entries.forEach((entry) => {
 			if (entry.isIntersecting) {
@@ -41,5 +12,4 @@ document.addEventListener("DOMContentLoaded", function () {
 	infoBoxes.forEach((infoBox) => {
 		observer.observe(infoBox);
 	});
-
 });
